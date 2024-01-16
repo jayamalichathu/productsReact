b8462bfb181cdd93bb246305342962a6f0935efe
jayamalichathu <jayamali85jaya@gmail.com>
2024-01-16 06:44:42 +0000

    do not show logs in the production

--- src/index.js
+++ src/index.js
@@ -6,6 +6,12 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import store from './stores/store';
 
+if (process.env.NODE_ENV === 'production') { //disable console.logs in production environment
+    console.log = () => {}
+    console.error = () => {}
+    console.debug = () => {}
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <Provider store={store}>
